09c4ea405177107b2df967777f4bbbf4e9cb681e
desisabela1919-ai <desisabela1919@gmail.com>
2026-06-18 17:18:58 +0700

    Create Firebase-config.js

---
+++ Firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+// ================================================================= */
+// KONFIGURASI DATABASE FIREBASE CLEAN ISABEL APP
+// ================================================================= */
+const firebaseConfig = {
+  apiKey: "AIzaSyBnXFEJjTovKQUGs74ZcziZ6odR6qxYeug",
+  authDomain: "clean-isabel-app-eaca6.firebaseapp.com",
+  databaseURL: "https://clean-isabel-app-eaca6-default-rtdb.firebaseio.com",
+  projectId: "clean-isabel-app-eaca6",
+  storageBucket: "clean-isabel-app-eaca6.firebasestorage.app",
+  messagingSenderId: "929083923327",
+  appId: "1:929083923327:web:3ebff1bbdd42eb3494984a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
